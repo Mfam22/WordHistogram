868b36e8ddc4f50c6681dc5cad38e646bc77fac7
Mfam22 <m.martinez1120@gmail.com>
2018-03-28 13:41:30 -0500

    Produce sorted results fixes #1

--- histogram/Histogram.java
+++ histogram/Histogram.java
@@ -30,11 +30,33 @@ public class Histogram{
 		file.close();
 	}
 	
+	public Object[][] generateList() {
+		Object[][] result = new Object[map.size()][2];
+		int i = 0;
+		for (String key : map.keySet()) {
+			Integer count = map.get(key);
+			result[i][0] = key;
+			result[i][1] = count;
+			i++;
+		}
+		Arrays.sort(result, new Comparator<Object[]>() {
+			@Override
+			public int compare(final Object[] ob1, final Object[] ob2) {
+				final Integer val1 = (Integer) ob1[1];
+				final Integer val2 = (Integer) ob2[1];
+				return val2.compareTo(val1);
+			}
+		});
+//		System.out.println(Arrays.deepToString(result));
+		return result;
+	}
+	
 	public void printMap() throws FileNotFoundException{
 		PrintWriter out = new PrintWriter(new FileOutputStream("output.txt"));
-		
-		for (String key : map.keySet()){
-			Integer count = map.get(key);
+		Object[][] sorted = generateList();
+		for (Object[] inner : sorted){
+			String key = (String) inner[0];
+			Integer count = (Integer) inner[1];
 			out.print(key + " | ");
 			for (int i=0;i<count;i++) {
 				out.print("=");
